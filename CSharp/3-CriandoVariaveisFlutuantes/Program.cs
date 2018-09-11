fdc92ce968d43e3ca2464a0749e5eb7b7986af4e
Tayguara Acioli <tayguaracioli@gmail.com>
2018-09-10 23:25:09 -0700

    Add files via upload
    Hello World;
    Creating Variable;
    Creating Floats;
    Cast and Different types of Variables;

---
+++ CSharp/3-CriandoVariaveisFlutuantes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisFlutuantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 3 - Criando Variaveis Flutuantes.");
+
+            double salario = 1200.70;
+
+            Console.WriteLine("O Seu salario e: " + salario + "!");
+
+            double idade;
+            idade = 15.0 / 2;
+
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5 / 3.0;
+
+            Console.WriteLine("5 / 3 = " + idade);
+
+            Console.WriteLine("A execucao acabou, pressione enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 4 - Conversoes e outros Tipos Numericos.");
+
+            double salario = 1200.50;
+
+            //O int eh uma variavel de 32 bits.
+            int salarioEmInteiro = (int)salario;
+
+            Console.WriteLine("Seu salario em double: " + salario + ". Seu salario em inteiro: " + salarioEmInteiro + ".");
+
+            //O long eh uma variavel de 64 bits.
+            long idadeLong = 13000000000;
+
+            Console.WriteLine(idadeLong);
+
+            //O short eh uma variavel de 16 bits.
+            short quantidadeProdutos = 150;
+            Console.WriteLine(quantidadeProdutos);
+
+            //Sempre que for usar float, por f apos numero ou o compilador vai confundir com double.
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.WriteLine("A execucao acabou, pressione enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
